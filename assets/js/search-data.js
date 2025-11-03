698a58a1c73a343f5d07ec9b0361c67f92fce54a
v-upadhyay <96621479+v-upadhyay@users.noreply.github.com>
2025-11-03 22:18:23 +0000

    Deploying to gh-pages from @ v-upadhyay/v-upadhyay.github.io@b643c3e418608cb0a53109cc2b1d9f81d48c89c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,26 +23,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
-          },
-        },{id: "nav-people",
-          title: "people",
-          description: "members of the lab or group",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/people/";
           },
         },{id: "post-a-post-with-plotly-js",
         
