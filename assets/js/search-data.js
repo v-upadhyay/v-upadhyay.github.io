35ecf98dd64c6ec1cbc8ca8a8009973d16a90d5f
v-upadhyay <96621479+v-upadhyay@users.noreply.github.com>
2025-11-16 13:14:38 +0000

    Deploying to gh-pages from @ v-upadhyay/v-upadhyay.github.io@ba0d48a42cf5e19285c35bd30f50a1407938c3da 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "Projects",
-          description: "A growing collection of your cool projects.",
+          description: "A collection of your projects.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
