ddf3864d8d7536a214dc35b54bd6f32660978d3b
v-upadhyay <96621479+v-upadhyay@users.noreply.github.com>
2025-12-31 11:30:38 +0000

    Deploying to gh-pages from @ v-upadhyay/v-upadhyay.github.io@1a66a81fad351fe00c08dee9b30de2cb8f241fbd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -466,35 +466,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%76%61%69%62%68%61%76.%75@%69%69%74%62.%61%63.%69%6E", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=gcfRoAQAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
